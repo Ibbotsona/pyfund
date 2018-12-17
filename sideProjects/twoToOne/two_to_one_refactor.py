fa6c07c76abf71d6271f37428aa5b38ddf575d21
Ant <antony.ibbotson1@nhs.net>
2018-12-17 19:18:17 +0000

    amend refactor to only include characters a to z

--- sideProjects/twoToOne/two_to_one_refactor.py
+++ sideProjects/twoToOne/two_to_one_refactor.py
@@ -1,8 +1,13 @@
-def longest(s1, s2):
-    string = "".join(sorted(set(s1 + s2)))
-    legal_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                     'v', 'w', 'x', 'y', 'z']
-    a_to_z_only = [letter for letter in string if letter in legal_letters]
+import string
 
-    return "".join(a_to_z_only)
+
+def longest(s1, s2):
+    sorted_str = sorted(set(s1 + s2))
+    # removes duplicates and sorts list
+    a_to_z_only = "".join([letter for letter in sorted_str if
+                           letter in string.ascii_lowercase])
+    # creates new list with only characters a to z
+    return a_to_z_only
+
+
+print(longest("sdsafsadslnsdnc34", "dvnlkdJJSDKD87"))
